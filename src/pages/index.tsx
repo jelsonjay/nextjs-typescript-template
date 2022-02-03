9adc78f93b813278a0aafb9dfec7b061029ba2b5
Jelson J <jelson20@outlook.com>
2022-02-03 10:57:50 +0000

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,12 @@
 import type { NextPage } from 'next';
 import { HomeContainer } from '../styles/HomeStyles';
-import Header from '../components/Header/index';
-import HomeHero from '../components/HomeHero/index';
-import Experience from '../components/Experience';
-import Projects from '../components/Projects';
-import Skills from '../components/Skills';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
+
 const Home: NextPage = () => {
 	return (
 		<HomeContainer>
-			<Header />
 			<main className='container'>
-				<HomeHero />
-				<Experience />
-				<Projects />
-				<Skills />
-				<Contact />
+				<h1>Next.js and Typescript Boilerplate</h1>
 			</main>
-			<Footer />
 		</HomeContainer>
 	);
 };
